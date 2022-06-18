3c25beacb914c81c7a4d6a6941615b3478610759
tiagolofi <jejeco75@gmail.com>
2022-06-18 12:04:20 -0300

    reqs and program

---
+++ pybov.py
@@ -0,0 +1,18 @@
+
+from monitoring import Monitoring
+
+while True:
+
+	name = input('Nome e ticker da empresa:\n')
+	tipo = input('Tipo ("acao" ou "bdr"):\n')
+	intervalo = int(input('Intervalo de tempo em segundos:\n'))
+	n_query = int(input('Quantidade de requisições:\n'))
+	var = input('Variável do gráfico:\n')
+
+	m = Monitoring(interval = intervalo, n_query = n_query, name = name, type_ = tipo)
+
+	m.delete_m()
+
+	m.apply_routine_query()
+
+	m.do_graph(variable = var)
